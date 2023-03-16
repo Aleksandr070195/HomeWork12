c2f0481d4b84fe04c24e0a1937f5ce338a2f468b
Aleksandr <07_sanek_95@mail.ru>
2023-03-16 13:52:11 +0300

    HW12

---
+++ src/Book.java
@@ -0,0 +1,25 @@
+public class Book {
+    String author;
+    String work;
+    int year;
+
+    public Book(String author, String work, int year) {
+        this.author = author;
+        this.work = work;
+        this.year = year;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public String getWork() {
+        return this.work;}
+
+    public int getYear() {
+        return this.year;}
+
+public void setYear(int year) {
+    this.year = year;
+}}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+        String[] author = {"А.С.Пушкин", "Ф.М.Достоевский"};
+        String[] work = {"Евгений Онегин", "Преступление и наказание"};
+        int[] year = {1833, 1866};
+        for (int i = 0; i < author.length; i++) {
+            for (int j = 0; j < work.length; j++) {
+                for (int k = 0; k < year.length; k++) {
+                }
+            }
+            System.out.println("писатель- "+ author[i] + ", произведение- " + work[i] + ", год- " + year[i]);
+        }
+
+        Book Pushkin = new Book("А.С.Пушкин", "Евгений Онегин", 1833 );
+        System.out.println("Pushkin.Author() = " + Pushkin.getAuthor());
+        System.out.println("Pushkin.Work() = " + Pushkin.getWork());
+        System.out.println("Pushkin.Year() = " + Pushkin.getYear());
+        Pushkin.setYear(2023);
+        System.out.println("Pushkin.getYear() = " + Pushkin.getYear());
+        Book Dostoevsky = new Book("Ф.М.Достоевский","Преступление и наказание", 1866 );
+        System.out.println("Dostoevsky.Author() = " + Dostoevsky.getAuthor());
+        System.out.println("Dostoevsky.Work() = " + Dostoevsky.getWork());
+        System.out.println("Dostoevsky.Year() = " + Dostoevsky.getYear());
+    }}
